50f8840175b4dc91eabebb0c08f06ff2f0022f7c
mastermaiknemo <kwidzinskimichael@gmail.com>
2016-10-16 20:05:13 +0200

    Ex

--- learnguage.sh
+++ learnguage.sh
@@ -9,8 +9,8 @@ while true;do
       copycopy=$(echo `xclip -selection primary -o`) 
         if [ $copy != $copycopy ]; then
           word=$(gawk -f <(curl -Ls git.io/translate) -show-original n -show-original-phonetics n -show-prompt-message n -show-languages n -show-alternatives n -show-dictionary n -no-ansi en:de `xclip -selection primary -o`)
-notify-send "$copycopy|$word"
-echo "$copycopy$word"  >> translations
+notify-send "$copycopy | $word" #under OSX replace this line with: "growlnotify -m "$copycopy | $word" -n "EasyReading"
+echo "$copycopy|$word"  >> translations
             break 1 
         fi
     ;done
