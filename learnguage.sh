839f2884b5c77b3b529491188fe5e2536ea92995
mastermaiknemo <kwidzinskimichael@gmail.com>
2016-10-16 20:00:54 +0200

    Script: Easy reading

---
+++ learnguage.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+#Sun Oct 16 19:29:58 CEST 2016i
+#magicminski
+file=translations
+( [ -e "$file" ] || touch "$file" ) && [ ! -w "$file" ] && echo cannot write to $file && exit 1
+while true;do
+  copy=$(echo `xclip -selection primary -o`)
+    while true; do
+      copycopy=$(echo `xclip -selection primary -o`) 
+        if [ $copy != $copycopy ]; then
+          word=$(gawk -f <(curl -Ls git.io/translate) -show-original n -show-original-phonetics n -show-prompt-message n -show-languages n -show-alternatives n -show-dictionary n -no-ansi en:de `xclip -selection primary -o`)
+notify-send "$copycopy|$word"
+echo "$copycopy$word"  >> translations
+            break 1 
+        fi
+    ;done
+;done
